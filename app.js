4dd99610ac4d0c402729f1bf0a5ba48b702234d7
Nick Frichette <frichetten@gmail.com>
2017-11-27 20:56:20 -0600

    I made some Improvements with the protected routes. I also built the actual Angular App and checked that it can be used with the express server. It can :)

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const users = require('./routes/users');
 const port = 8080;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
 // Passport
@@ -37,6 +37,9 @@ app.get('/', (req, res) => {
  res.send("How's it going");
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(port, () => {
   console.log('Server started on port ' + port);
